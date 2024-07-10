a102e7161e4a2a82ed197ee2dbea0838d3fc9794
Cahya Wirawan <cahya.wirawan@gmail.com>
2024-07-11 01:30:17 +0200

    add files

---
+++ src/main.rs
@@ -0,0 +1,88 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader, BufWriter, Write};
+use rwkv_tokenizer;
+use serde::Deserialize;
+use bytemuck::cast_slice;
+use clap::Parser;
+
+const HDR_MAGIC: &str = "MMIDIDX\x00\x00";
+const VERSION: [u8; 8] = [1u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8];
+const DTYPE: [u8; 1] = [8u8];
+#[derive(Deserialize, Debug)]
+struct Jsonline {
+    text: String
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Jsonlines file to read
+    #[arg(short, long)]
+    input: std::path::PathBuf,
+
+    /// Output directory for binidx files
+    #[arg(short, long, required = false, default_value = "-")]
+    output_dir: std::path::PathBuf,
+}
+
+
+fn main() {
+    println!("Json converter to RWKV binidx file format");
+    let args = Args::parse();
+    let tokenizer = rwkv_tokenizer::WorldTokenizer::new(None).unwrap();
+
+    let file_in = File::open(args.input.clone()).expect("couldn't open file");
+    let filename = args.input.file_name().unwrap();
+    let output_dir;
+    output_dir = if args.output_dir.to_str().unwrap() == "-" {
+        args.input.parent().unwrap()
+    } else {
+        &*args.output_dir
+    };
+    output_dir
+    let mut file_bin = output_dir.join(filename);
+    file_bin.set_extension("bin");
+    let mut file_idx = output_dir.join(filename);
+    file_idx.set_extension("idx");
+
+    let file_bin = File::create(file_bin).expect("couldn't open file");
+    let file_idx = File::create(file_idx).expect("couldn't open file");
+    let mut bytes_counter = 0;
+    let mut tokens_counter = 0;
+    let mut doc_length: u64 = 0;
+    let mut doc_sizes: Vec<u32> = Vec::new();
+    let mut doc_pointers: Vec<u64> = vec![0u64];
+    let mut doc_indexes: Vec<u64> = vec![0u64];
+    let mut file_bin_writer = BufWriter::new(file_bin);
+    let mut file_idx_writer = BufWriter::new(file_idx);
+    for line in BufReader::new(file_in).lines() {
+        doc_length += 1;
+        let line = line.expect("couldn't get line");
+        bytes_counter += line.len();
+        let ds: Jsonline = serde_json::from_str(&line).unwrap();
+        let mut token_ids = tokenizer.encode(ds.text.as_str());
+        token_ids.push(0);
+        let token_bytes: &[u8] = cast_slice(&token_ids);
+        file_bin_writer.write(token_bytes).expect("Can't write");
+        tokens_counter += token_ids.len();
+        doc_sizes.push(token_ids.len() as u32);
+        let last_pointer = doc_pointers[doc_pointers.len()-1];
+        doc_pointers.push(last_pointer + 2*token_ids.len() as u64);
+        doc_indexes.push(doc_indexes[doc_indexes.len()-1] + 1);
+    }
+    file_bin_writer.flush().unwrap();
+
+    doc_pointers.pop();
+    file_idx_writer.write(HDR_MAGIC.as_bytes()).expect("Can't write");
+    file_idx_writer.write(cast_slice(&VERSION)).expect("Can't write");
+    file_idx_writer.write(cast_slice(&DTYPE)).expect("Can't write");
+    file_idx_writer.write(cast_slice(&[doc_length])).expect("Can't write");
+    file_idx_writer.write(cast_slice(&[doc_length+1])).expect("Can't write");
+    file_idx_writer.write(cast_slice(&doc_sizes)).expect("Can't write");
+    file_idx_writer.write(cast_slice(&doc_pointers)).expect("Can't write");
+    file_idx_writer.write(cast_slice(&doc_indexes)).expect("Can't write");
+    file_idx_writer.flush().unwrap();
+    println!("Output directory: {:?}", output_dir);
+    println!("Bytes read: {:?}", bytes_counter);
+    println!("Tokens written: {:?}", tokens_counter);
+}
