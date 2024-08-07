5c03c63e83dac9206908620308312a179a8a4183
Cahya Wirawan <cahya.wirawan@gmail.com>
2024-08-07 14:01:38 +0200

    add multithreading

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use std::ffi::c_float;
-use std::fs;
-use std::fs::File;
-use std::io::{BufRead, BufReader, BufWriter, Write};
-use rwkv_tokenizer;
-use serde::Deserialize;
-use bytemuck::cast_slice;
+use std::fs::{File, remove_file, rename};
+use std::{cmp, thread};
+use std::sync::mpsc;
+use std::io::{BufRead, BufReader, BufWriter, Seek, Write, SeekFrom};
+use std::sync::mpsc::Sender;
+use std::collections::HashMap;
+use std::path::PathBuf;
+use std::time::Instant;
 use clap::Parser;
 use tqdm::pbar;
-use std::time::Instant;
-use std::cmp;
+use serde::Deserialize;
+use bytemuck::cast_slice;
+use std::io::prelude::*;
+use rwkv_tokenizer;
 
+const MAX_THREADS: u16 = 5;
 const MAGIC_HDR: &str = "MMIDIDX\x00\x00";
 const VERSION: [u8; 8] = [1u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8];
 const DTYPE: [u8; 1] = [8u8];
@@ -31,84 +36,202 @@ struct Args {
     output_dir: std::path::PathBuf,
 }
 
+#[derive(Debug)]
+struct Metadata {
+    name: String,
+    index: u16,
+    doc_length: u64,
+    doc_sizes: Vec<u32>,
+    bytes_counter: usize,
+    tokens_counter: usize
+}
+
+
+fn json2bin(thread_index: u16, max_threads: u16, tx: Sender<Metadata>, filename: &str) {
+    let tokenizer = rwkv_tokenizer::WorldTokenizer::new(None).unwrap();
+    let input  = PathBuf::from(filename);
+    let input_ = input.clone();
+    let output_dir = input_.parent().unwrap();
+    let mut file_in = File::open(input.clone()).expect("couldn't open file");
+    let filename = format!("{}_{}", input.file_stem().unwrap().to_str().unwrap(), thread_index);
+    let file_path_bin = output_dir.join(format!("{filename}.bin"));
+    let file_bin = File::create(file_path_bin).expect("couldn't open file");
+
+    let mut doc_length: u64 = 0;
+    let mut doc_sizes: Vec<u32> = Vec::new();
+    let mut file_bin_writer = BufWriter::new(file_bin);
+
+    let file_size = file_in.metadata().unwrap().len();
+    let start_position = file_size/max_threads as u64 *thread_index as u64;
+    let current_position = file_in.seek(SeekFrom::Start(start_position)).unwrap();
+    // println!("start_position: {start_position}, current_position: {current_position}");
+    assert_eq!(start_position, current_position);
+    let mut line_counter = 0;
+    let mut bytes_counter = 0;
+    let mut tokens_counter = 0;
+    let mut file_size_per_thread = file_size/max_threads as u64;
+    let mut pbar = pbar(Some(file_size_per_thread as usize));
+    for line in BufReader::new(file_in).lines() {
+        let line = line.expect("couldn't get line");
+        let line_length = line.len();
+        if file_size_per_thread > (line_length+1) as u64 {
+            pbar.update(line_length+1).unwrap();
+            file_size_per_thread -= (line_length+1) as u64;
+        } else {
+            pbar.update(file_size_per_thread as usize).unwrap();
+            file_size_per_thread = 0;
+        }
+        bytes_counter += line_length;
+        line_counter += 1;
+        if line_counter == 1 && thread_index != 0 {
+            // println!("jsonl {thread_index}:: {:?}", line);
+            continue;
+        }
+
+        doc_length += 1;
+        let ds: Jsonline = serde_json::from_str(&line).unwrap();
+        // println!("jsonl {thread_index}: {:?}", ds);
+        let mut token_ids = tokenizer.encode(ds.text.as_str());
+        token_ids.push(0);
+        tokens_counter += token_ids.len();
+        let token_bytes: &[u8] = cast_slice(&token_ids);
+        file_bin_writer.write(token_bytes).expect("Can't write");
+        doc_sizes.push(token_ids.len() as u32);
+        if bytes_counter as u64> file_size/max_threads as u64 {
+            // println!("jsonl {thread_index}: End {bytes_read}:{:?}", file_size/MAX_THREADS as u64);
+            break;
+        }
+    }
+    file_bin_writer.flush().unwrap();
+    let metadata = Metadata {
+        name: format!("Thread: {thread_index}"),
+        index: thread_index,
+        doc_length: doc_length,
+        doc_sizes: doc_sizes,
+        bytes_counter: bytes_counter,
+        tokens_counter: tokens_counter
+    };
+    tx.send(metadata).unwrap();
+}
 
 fn main() {
+    // let mut handles = vec![];
+    let (tx, rx) = mpsc::channel();
+    let mut results: HashMap<u16, Metadata> = HashMap::new();
     println!("Json converter to RWKV binidx file format");
     let args = Args::parse();
-    let tokenizer = rwkv_tokenizer::WorldTokenizer::new(None).unwrap();
 
-    //let input = args.input.clone();
-    let file_in = File::open(&args.input).expect("couldn't open file");
-    let filename = &args.input.file_name().unwrap();
+    let root_filename = args.input.file_stem().unwrap().to_str().unwrap();
+    let filename = args.input.to_str().unwrap();
+    //let filename = args.input.file_name().unwrap().to_str().unwrap();
+    /*
+    let filename = {
+        let input = &args.input;
+        input.file_name()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .to_owned()
+    };
+    */
+    // let root_filename = filename.file;
+    // let filename = filename.as_str();
+
     let output_dir;
     output_dir = if args.output_dir.to_str().unwrap() == "-" {
         &args.input.parent().unwrap()
     } else {
         &*args.output_dir
     };
-    let metadata = fs::metadata(&args.input);
-    let filesize = metadata.unwrap().len();
-    let mut file_bin = output_dir.join(filename);
-    file_bin.set_extension("bin");
-    let mut file_idx = output_dir.join(filename);
-    file_idx.set_extension("idx");
-
-    let file_bin = File::create(file_bin).expect("couldn't open file");
-    let file_idx = File::create(file_idx).expect("couldn't open file");
-    let mut bytes_counter = 0;
-    let mut tokens_counter = 0;
-    let mut doc_length: u64 = 0;
-    let mut doc_sizes: Vec<u32> = Vec::new();
-    let mut doc_pointers: Vec<u64> = vec![0u64];
-    let mut doc_indexes: Vec<u64> = vec![0u64];
-    let mut file_bin_writer = BufWriter::new(file_bin);
-    let mut file_idx_writer = BufWriter::new(file_idx);
-    let mut pbar = pbar(Some(filesize as usize));
     let start = Instant::now();
-    for line in BufReader::new(file_in).lines() {
-        doc_length += 1;
-        let line = line.expect("couldn't get line");
-        let line_length = line.len();
-        pbar.update(line_length+1).unwrap();
-        bytes_counter += line_length;
-        let ds: Jsonline = serde_json::from_str(&line).unwrap();
-        let mut token_ids = tokenizer.encode(ds.text.as_str());
-        token_ids.push(0);
-        let token_bytes: &[u8] = cast_slice(&token_ids);
-        file_bin_writer.write(token_bytes).expect("Can't write");
-        tokens_counter += token_ids.len();
-        doc_sizes.push(token_ids.len() as u32);
-        let last_pointer = doc_pointers[doc_pointers.len()-1];
-        doc_pointers.push(last_pointer + 2*token_ids.len() as u64);
-        doc_indexes.push(doc_indexes[doc_indexes.len()-1] + 1);
+    thread::scope(|scope| {
+        for thread_index in 0..MAX_THREADS {
+            let tx_ = tx.clone();
+            scope.spawn(move || {
+                json2bin(thread_index, MAX_THREADS, tx_, filename);
+            });
+            // handles.push(handle);
+        }
+    });
+    for (index, metadata) in rx.iter().enumerate() {
+        println!("{index} Got: {:?} - {:?} - {:?}", metadata.index, metadata.name, metadata.doc_length);
+        println!("doc_sizes: {:?}", metadata.doc_sizes);
+        results.insert(metadata.index, metadata);
+        if index as u16 >= (MAX_THREADS - 1) {
+            break;
+        }
     }
-    file_bin_writer.flush().unwrap();
-    let elapsed = start.elapsed();
+    println!("End received");
+    let mut document_length_all = 0;
+    let mut bytes_counter_all= 0;
+    let mut tokens_counter_all= 0;
+    // let mut index:u16 = 0;
+    for index in 0..MAX_THREADS {
+        // handle.join().unwrap();
+        println!("result {index}: {:?}", results[&index]);
+        document_length_all += results[&index].doc_length;
+        bytes_counter_all += results[&index].bytes_counter;
+        tokens_counter_all += results[&index].tokens_counter;
+        // index += 1;
+    }
 
-    doc_pointers.pop();
-    file_idx_writer.write(MAGIC_HDR.as_bytes()).expect("Can't write");
-    file_idx_writer.write(cast_slice(&VERSION)).expect("Can't write");
-    file_idx_writer.write(cast_slice(&DTYPE)).expect("Can't write");
-    file_idx_writer.write(cast_slice(&[doc_length])).expect("Can't write");
-    file_idx_writer.write(cast_slice(&[doc_length+1])).expect("Can't write");
-    for i in (0..doc_sizes.len()).step_by(VEC_STEP) {
-        file_idx_writer.write(cast_slice(&doc_sizes[i..cmp::min(i+VEC_STEP, doc_sizes.len())])).expect("Can't write");
+    let file_idx = output_dir.join(format!("{root_filename}.idx"));
+    let file_idx_out = File::create(file_idx).expect("couldn't open file");
+    let mut buf_idx_writer: BufWriter<File> = BufWriter::new(file_idx_out);
+    buf_idx_writer.write(MAGIC_HDR.as_bytes()).expect("Can't write");
+    buf_idx_writer.write(cast_slice(&VERSION)).expect("Can't write");
+    buf_idx_writer.write(cast_slice(&DTYPE)).expect("Can't write");
+    buf_idx_writer.write(cast_slice(&[document_length_all])).expect("Can't write");
+    buf_idx_writer.write(cast_slice(&[document_length_all+1])).expect("Can't write");
+
+    for index in 0..MAX_THREADS {
+        for i in (0..results[&index].doc_sizes.len()).step_by(VEC_STEP) {
+            buf_idx_writer.write(cast_slice(&results[&index].doc_sizes[i..cmp::min(i+VEC_STEP, results[&index].doc_sizes.len())])).expect("Can't write");
+        }
     }
-    for i in (0..doc_pointers.len()).step_by(VEC_STEP) {
-        file_idx_writer.write(cast_slice(&doc_pointers[i..cmp::min(i+VEC_STEP, doc_pointers.len())])).expect("Can't write");
+    buf_idx_writer.flush().unwrap();
+    buf_idx_writer.write(cast_slice(&[0u64])).expect("Can't write");
+    let mut last_pointer: u64 = 0;
+    for index in 0..MAX_THREADS {
+        for i in 0..results[&index].doc_sizes.len() {
+            if (index == MAX_THREADS - 1) && (i == results[&index].doc_sizes.len() - 1) {
+                break;
+            }
+            let pointer = last_pointer + 2 * results[&index].doc_sizes[i] as u64;
+            buf_idx_writer.write(cast_slice(&[pointer])).expect("Can't write");
+            last_pointer = pointer;
+        }
     }
-    for i in (0..doc_indexes.len()).step_by(VEC_STEP) {
-        file_idx_writer.write(cast_slice(&doc_indexes[i..cmp::min(i+VEC_STEP, doc_indexes.len())])).expect("Can't write");
+    for i in 0..document_length_all+1 {
+        buf_idx_writer.write(cast_slice(&[i as u64])).expect("Can't write");
     }
-    file_idx_writer.flush().unwrap();
-    let mut filename_bin = filename.to_str().unwrap().replace(".jsonl", "").to_owned();
-    filename_bin.push_str(".bin");
-    let mut filename_idx = filename.to_str().unwrap().replace(".jsonl", "").to_owned();
-    filename_idx.push_str(".idx");
-    println!("- Output files:  {}/{{{filename_bin},{filename_idx}}}", output_dir.to_str().unwrap());
-    println!("- Bytes read: {:?}", bytes_counter);
-    println!("- Tokens written: {:?}", tokens_counter);
-    println!("- Bytes/tokens: {:?}", bytes_counter as c_float/tokens_counter as c_float);
+    buf_idx_writer.flush().unwrap();
+
+    let file_bin = output_dir.join(format!("{root_filename}_0.bin"));
+    let file_bin_out = File::options().append(true)
+        .open(file_bin.clone()).expect("couldn't open file");
+    let mut buf_bin_writer: BufWriter<File>;
+    buf_bin_writer = BufWriter::new(file_bin_out);
+    for index in 0..MAX_THREADS {
+        if index == 0 {
+            continue;
+        } else {
+            let file_x_bin = output_dir.join(format!("{root_filename}_{index}.bin"));
+            let file_bin_in = File::open(file_x_bin.clone()).expect("couldn't open file");
+            let file_size = file_bin_in.metadata().unwrap().len();
+            let buf_reader = BufReader::new(file_bin_in);
+            let mut part_reader = buf_reader.take(file_size);
+            std::io::copy(&mut part_reader, &mut buf_bin_writer).unwrap();
+            buf_bin_writer.flush().unwrap();
+            remove_file(file_x_bin).expect("couldn't remove file");
+        }
+    }
+    rename(file_bin, output_dir.join(format!("{root_filename}.bin"))).unwrap();
+    let elapsed = start.elapsed();
+    println!("- Output files:  {}/{{{root_filename}.bin,{root_filename}.idx}}", output_dir.to_str().unwrap());
+    println!("- Bytes read: {:?}", bytes_counter_all);
+    println!("- Tokens written: {:?}", tokens_counter_all);
+    println!("- Bytes/tokens: {:?}", bytes_counter_all as c_float/tokens_counter_all as c_float);
     println!("- Elapsed time: {:.2?}", elapsed);
-    println!("- Performance: {:.2?}MB/s", bytes_counter as f32/elapsed.as_secs_f32()/(1024*1024) as f32);
+    println!("- Performance: {:.2?}MB/s", bytes_counter_all as f32/elapsed.as_secs_f32()/(1024*1024) as f32);
 }
